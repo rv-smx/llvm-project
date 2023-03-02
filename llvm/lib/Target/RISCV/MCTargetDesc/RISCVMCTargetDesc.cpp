686de674ba98868e667ce91c103a7023dc7be6f0
MaxXSoft <x@MaxXSoft.net>
2023-03-02 18:21:16 +0800

    [RISCV] Remove additional checks in `RISCVMCInstrAnalysis`.

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCTargetDesc.cpp
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVMCTargetDesc.cpp
@@ -140,13 +140,6 @@ public:
       return true;
     }
 
-    if (Inst.getOpcode() == RISCV::SMX_STEP_BL ||
-        Inst.getOpcode() == RISCV::SMX_STEP_J ||
-        Inst.getOpcode() == RISCV::SMX_BNL) {
-      Target = Addr + Inst.getOperand(2).getImm();
-      return true;
-    }
-
     return false;
   }
 };
